dd1053ad738d516d93edcd53f4d42030d1b3ac85
Leeanne McManus <leeanne@ikuwsolutions.com>
2020-10-28 18:53:33 -0600

    updated solution

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ function addContact(event) {
  */
 function saveContacts() {
   window.localStorage.setItem("contacts", JSON.stringify(contacts))
+  drawContacts()
 }
 
 /**
@@ -50,9 +51,28 @@ function loadContacts() {
  * This function targets the contacts-list on the 
  * DOM and adds a new div element for each of the
  * contacts in the contacts array
+ * Uses JS ternary (if this is true, then add er contact, if false, then don't have that class)
  */
 function drawContacts() {
-  
+  let contactListElement = document.getElementById("contact-list")
+  let contactsTemplate = ""
+
+  contacts.forEach(contact =>{
+    contactsTemplate += `
+    <div class = "contact-card" ${contact.emergencyContact ? 'emergency-contact' : ''}">
+    <h3 class="mt-1 mb-1">${contact.name}</h3>
+    <div class="d-flex space-between">
+      <p>
+       <i class="fa fa-fw fa-phone"></i>
+       <span>${contact.phone}</span>
+      </p>
+      <i class="action fa fa-trash-danger text-danger" onclick="removeContact(${contact.id}')
+      "></i>
+      // <button type="button" onclick="removeContact(${contact.id}')"></i>
+    </div>
+    `
+  })
+  contactListElement.innerHTML = contactsTemplate
 }
 
 /**
@@ -65,6 +85,12 @@ function drawContacts() {
  * @param {string} contactId 
  */
 function removeContact(contactId) {
+  let index = contacts.findIndex(contact => contact.id == contactId)
+  if(index == -1){
+    throw new Error("Invalid Contact Id")
+  }
+  contacts.splice(index, 1)
+  saveContacts()
 }
 
 /**
